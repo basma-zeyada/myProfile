7997ee0f0a02b4f9456798d3af3e527abd0da4d9
Basma Zeyada <basma.zeyada@espace.com.eg>
2021-03-24 09:09:00 +0200

    fix script import issue

--- js/vendors.js
+++ js/vendors.js
@@ -1,2 +1,2 @@
-import('../node_modules/jquery/dist/jquery.js');
-import('../node_modules/bootstrap/dist/js/bootstrap.bundle.js');
+import('https://code.jquery.com/jquery-3.6.0.min.js');
+import('../node_modules/bootstrap/dist/js/bootstrap.js');
